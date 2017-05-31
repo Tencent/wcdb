536f9dcb38a42506e44eccc081215fe585354749
sanhuazhang <sanhuazhang@tencent.com>
2017-05-31 14:37:44 +0800

    Sample repair

---
+++ apple/sample/repair/sample_repair_main.h
@@ -0,0 +1,11 @@
+//
+//  sample_repair_main.h
+//  WCDB
+//
+//  Created by sanhuazhang on 2017/5/31.
+//  Copyright © 2017年 sanhuazhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void sample_repair_main(NSString* baseDirectory);
