7c71c8001ab2175ddba63810c23cf45062a7b210
sanhuazhang <sanhuazhang@tencent.com>
2017-11-20 11:43:38 +0800

    Column is expression operatable

--- swift/source/abstract/Column.swift
+++ swift/source/abstract/Column.swift
@@ -43,7 +43,7 @@ public class Column: Describable {
     }
 }
 
-extension Column: ColumnConvertible {
+extension Column: ColumnConvertible, ExpressionOperable {
     public func asColumn() -> Column {
         return self
     }
