4d6e08c42bf8001ddf72628bc5da7fe3ab049cc4
maxu <mx.yolande@gmail.com>
2017-12-04 16:15:49 +0800

    fix wrong world

--- apple/xctemplate/WCDB/WCTTableCoding.xctemplate/___FILEBASENAME___.h
+++ apple/xctemplate/WCDB/WCTTableCoding.xctemplate/___FILEBASENAME___.h
@@ -6,7 +6,7 @@
 
 @property(nonatomic, retain) NSString *<#property1#>;
 @property(nonatomic, assign) NSInteger <#property2#>;
-@property(nonatomic, assign) CGFlot <#property3#>;
+@property(nonatomic, assign) float <#property3#>;
 @property(nonatomic, strong) NSArray *<#property4#>;
 @property(nonatomic, readonly) NSDate *<#..........#>;
 
