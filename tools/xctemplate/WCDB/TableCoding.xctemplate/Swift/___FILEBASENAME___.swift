c0000a96d6b55969e7086fe6f050ba5306c1d939
sanhuazhang <sanhuazhang@tencent.com>
2017-12-14 23:09:31 +0800

    Update xctemplate

--- tools/xctemplate/WCDB/TableCoding.xctemplate/Swift/___FILEBASENAME___.swift
+++ tools/xctemplate/WCDB/TableCoding.xctemplate/Swift/___FILEBASENAME___.swift
@@ -26,7 +26,7 @@ class ___VARIABLE_productName___: WCDB.TableCodable {
         static let objectRelationalMapping = TableBinding(CodingKeys.self)
 
         //Column constraints for primary key, unique, not null, default value and so on. It is optional.
-        //static var __columnConstraintBindings: [CodingKeys: ColumnConstraintBinding]? {
+        //static var columnConstraintBindings: [CodingKeys: ColumnConstraintBinding]? {
         //    return [
         //        .variable: ColumnConstraintBinding(isPrimary: true, isAutoIncrement: true),
         //        .variable2: ColumnConstraintBinding(isUnique: true)
@@ -34,21 +34,21 @@ class ___VARIABLE_productName___: WCDB.TableCodable {
         //}
 
         //Index bindings. It is optional.
-        //static var __indexBindings: [IndexBinding.Subfix: IndexBinding]? {
+        //static var indexBindings: [IndexBinding.Subfix: IndexBinding]? {
         //    return [
         //        "_index": IndexBinding(indexesBy: CodingKeys.variable2)
         //    ]
         //}
 
         //Table constraints for multi-primary, multi-unique and so on. It is optional.
-        //static var __tableConstraintBindings: [TableConstraintBinding.Name: TableConstraintBinding]? {
+        //static var tableConstraintBindings: [TableConstraintBinding.Name: TableConstraintBinding]? {
         //    return [
         //        "MultiPrimaryConstraint": MultiPrimaryBinding(indexesBy: variable2.asIndex(orderBy: .descending), variable3.primaryKeyPart2)
         //    ]
         //}
 
         //Virtual table binding for FTS and so on. It is optional.
-        //static var __virtualTableBinding: VirtualTableBinding? {
+        //static var virtualTableBinding: VirtualTableBinding? {
         //    return VirtualTableBinding(with: .fts3, and: ModuleArgument(with: .WCDB))
         //}
     }
