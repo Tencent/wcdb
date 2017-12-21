d4e1cc155a12739a7cf8d291e9511fea2795c352
sanhuazhang <sanhuazhang@tencent.com>
2017-12-21 12:04:03 +0800

    Replace tabs with spaces

--- tools/templates/install.sh
+++ tools/templates/install.sh
@@ -2,22 +2,22 @@
 
 #xctemplate
 install_xctemplates() {
-	templates=~/Library/Developer/Xcode/Templates
-	file_templates=~/Library/Developer/Xcode/Templates/File\ Templates
-	source_templates=`pwd`/xctemplates/
+    templates=~/Library/Developer/Xcode/Templates
+    file_templates=~/Library/Developer/Xcode/Templates/File\ Templates
+    source_templates=`pwd`/xctemplates/
 
-	if [ -f $templates ] ;
-	then
-		rm $templates
-	fi;
-	mkdir -p "$file_templates"
-	cp -R $source_templates "$file_templates"
+    if [ -f $templates ] ;
+    then
+        rm $templates
+    fi;
+    mkdir -p "$file_templates"
+    cp -R $source_templates "$file_templates"
 }
 
 install_codesnippets() {
-	source_codesnippets=`pwd`/codesnippets/*.codesnippet
-	codesnippets=~/Library/Developer/Xcode/UserData/CodeSnippets/
-	cp $source_codesnippets $codesnippets
+    source_codesnippets=`pwd`/codesnippets/*.codesnippet
+    codesnippets=~/Library/Developer/Xcode/UserData/CodeSnippets/
+    cp $source_codesnippets $codesnippets
 }
 
 install_xctemplates
