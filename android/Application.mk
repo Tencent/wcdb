bb9830e2b22d23bba29358c4f2b4ba1679708e91
johnwhe <johnwhe@tencent.com>
2018-01-02 16:19:22 +0800

    Support building with GCC toolchain

--- android/Application.mk
+++ android/Application.mk
@@ -2,9 +2,12 @@ LOCAL_PATH:= $(call my-dir)
 
 NDK_PROJECT_PATH := $(LOCAL_PATH)/build
 APP_BUILD_SCRIPT := $(LOCAL_PATH)/Android.mk
-#NDK_TOOLCHAIN_VERSION := 4.9
 APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64
 APP_PLATFORM := android-14
 APP_STL := gnustl_static
 APP_PIE := true
 APP_DEPRECATED_HEADERS := true
+
+ifdef USE_GCC
+	NDK_TOOLCHAIN_VERSION := 4.9
+endif
