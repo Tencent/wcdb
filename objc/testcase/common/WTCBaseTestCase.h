169a50316e6efa5c01ba3eae2c0656fec84eb77a
sanhuazhang <sanhuazhang@tencent.com>
2018-01-18 10:55:30 +0800

    Add recommendTag for base test

--- objc/testcase/common/WTCBaseTestCase.h
+++ objc/testcase/common/WTCBaseTestCase.h
@@ -30,7 +30,7 @@
 
 @property(readonly) NSString *recommendedPath;
 
-//@property(readonly) WCTTag recommendTag;
+@property(readonly) WCTTag recommendTag;
 
 @property(readonly) NSFileManager *fileManager;
 
