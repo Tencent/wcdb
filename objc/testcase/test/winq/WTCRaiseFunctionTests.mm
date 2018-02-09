2764f183f5d549b0c29cf7a9c5c11e03df9ca16f
sanhuazhang <sanhuazhang@tencent.com>
2018-02-09 18:05:52 +0800

    Code formating

--- objc/testcase/test/winq/WTCRaiseFunctionTests.mm
+++ objc/testcase/test/winq/WTCRaiseFunctionTests.mm
@@ -30,13 +30,13 @@
 - (void)testRaiseFunction
 {
     std::string errorMessage = "testError";
-    
+
     WINQAssertEqual(WCDB::RaiseFunction().withIgnore(), @"RAISE(IGNORE)");
-    
+
     WINQAssertEqual(WCDB::RaiseFunction().withRollback(errorMessage), @"RAISE(ROLLBACK, testError)");
-    
+
     WINQAssertEqual(WCDB::RaiseFunction().withAbort(errorMessage), @"RAISE(ABORT, testError)");
-    
+
     WINQAssertEqual(WCDB::RaiseFunction().withFail(errorMessage), @"RAISE(FAIL, testError)");
 }
 
