52abbcad684afc427fd1a560008024c9fcf67c84
sanhuazhang <sanhuazhang@tencent.com>
2018-02-09 17:09:18 +0800

    Tests for JoinClause

--- objc/testcase/test/winq/WTCJoinClauseTests.mm
+++ objc/testcase/test/winq/WTCJoinClauseTests.mm
@@ -29,6 +29,29 @@
 
 - (void)testJoinClause
 {
+    WCDB::TableOrSubquery tableOrSubquery = "testTable";
+    WCDB::TableOrSubquery tableOrSubquery1 = "testTable1";
+    WCDB::JoinConstraint joinConstraint1 = WCDB::JoinConstraint().usingColumn("testColumn1");
+    WCDB::TableOrSubquery tableOrSubquery2 = "testTable2";
+    WCDB::JoinConstraint joinConstraint2 = WCDB::JoinConstraint().usingColumn("testColumn2");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery), @"testTable");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery).with(tableOrSubquery1, joinConstraint1), @"testTable, testTable1 USING(testColumn1)");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery).join(tableOrSubquery1, joinConstraint1), @"testTable JOIN testTable1 USING(testColumn1)");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery).leftOuterJoin(tableOrSubquery1, joinConstraint1), @"testTable LEFT OUTER JOIN testTable1 USING(testColumn1)");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery).leftJoin(tableOrSubquery1, joinConstraint1), @"testTable LEFT JOIN testTable1 USING(testColumn1)");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery).innerJoin(tableOrSubquery1, joinConstraint1), @"testTable INNER JOIN testTable1 USING(testColumn1)");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery).crossJoin(tableOrSubquery1, joinConstraint1), @"testTable CROSS JOIN testTable1 USING(testColumn1)");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery).leftOuterJoin(tableOrSubquery1, joinConstraint1), @"testTable LEFT OUTER JOIN testTable1 USING(testColumn1)");
+
+    WINQAssertEqual(WCDB::JoinClause(tableOrSubquery).with(tableOrSubquery1, joinConstraint1).join(tableOrSubquery2, joinConstraint2), @"testTable, testTable1 USING(testColumn1) JOIN testTable2 USING(testColumn2)");
 }
 
 @end
