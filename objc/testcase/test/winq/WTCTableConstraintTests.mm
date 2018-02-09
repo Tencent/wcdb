4b534bdee149dd67473a81666a8952199a92918f
sanhuazhang <sanhuazhang@tencent.com>
2018-02-09 18:25:46 +0800

    Tests for TableConstraint

--- objc/testcase/test/winq/WTCTableConstraintTests.mm
+++ objc/testcase/test/winq/WTCTableConstraintTests.mm
@@ -29,6 +29,30 @@
 
 - (void)testTableConstraint
 {
+    std::string name = "testConstraint";
+    std::string columnName1 = "testColumn1";
+    std::string columnName2 = "testColumn2";
+    std::string columnName3 = "testColumn3";
+    std::list<std::string> columnNames = {columnName1, columnName2};
+    WCDB::IndexedColumn indexedColumn1 = WCDB::IndexedColumn(columnName1);
+    std::list<WCDB::IndexedColumn> indexedColumns = {indexedColumn1, WCDB::IndexedColumn(columnName2)};
+    WCDB::Expression expression = WCDB::Expression(WCDB::Expression::ColumnNamed(columnName3)).notNull();
+    WCDB::ConflictClause conflictClause = WCDB::ConflictClause::Rollback;
+    WCDB::ForeignKeyClause foreignKeyClause = WCDB::ForeignKeyClause("testForeignTable");
+
+    WINQAssertEqual(WCDB::TableConstraint(name).withPrimaryKey(indexedColumn1), @"CONSTRAINT testConstraint PRIMARY KEY(testColumn1)");
+
+    WINQAssertEqual(WCDB::TableConstraint(name).withPrimaryKey(indexedColumn1, conflictClause), @"CONSTRAINT testConstraint PRIMARY KEY(testColumn1) ON CONFLICT ROLLBACK");
+
+    WINQAssertEqual(WCDB::TableConstraint(name).withPrimaryKey(indexedColumns), @"CONSTRAINT testConstraint PRIMARY KEY(testColumn1, testColumn2)");
+
+    WINQAssertEqual(WCDB::TableConstraint(name).withUnique(indexedColumn1), @"CONSTRAINT testConstraint UNIQUE(testColumn1)");
+
+    WINQAssertEqual(WCDB::TableConstraint(name).withChecking(expression), @"CONSTRAINT testConstraint CHECK(testColumn3 NOTNULL)");
+
+    WINQAssertEqual(WCDB::TableConstraint(name).withForeignKey(columnName1, foreignKeyClause), @"CONSTRAINT testConstraint FOREIGN KEY(testColumn1) REFERENCES testForeignTable");
+
+    WINQAssertEqual(WCDB::TableConstraint(name).withForeignKey(columnNames, foreignKeyClause), @"CONSTRAINT testConstraint FOREIGN KEY(testColumn1, testColumn2) REFERENCES testForeignTable");
 }
 
 @end
