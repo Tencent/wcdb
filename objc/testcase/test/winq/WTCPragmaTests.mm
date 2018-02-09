e0ec69ffca838e34324ce5f27dc7f6d374154bae
sanhuazhang <sanhuazhang@tencent.com>
2018-02-09 17:55:42 +0800

    Tests for Pragma

--- objc/testcase/test/winq/WTCPragmaTests.mm
+++ objc/testcase/test/winq/WTCPragmaTests.mm
@@ -27,8 +27,162 @@
 
 @implementation WTCPragmaTests
 
-- (void)testPragma
+- (void)testPragmaSQLite
 {
+    WINQAssertEqual(WCDB::Pragma::ApplicationId, @"application_id");
+
+    WINQAssertEqual(WCDB::Pragma::AutoVacuum, @"auto_vacuum");
+
+    WINQAssertEqual(WCDB::Pragma::AutomaticIndex, @"automatic_index");
+
+    WINQAssertEqual(WCDB::Pragma::BusyTimeout, @"busy_timeout");
+
+    WINQAssertEqual(WCDB::Pragma::CacheSize, @"cache_size");
+
+    WINQAssertEqual(WCDB::Pragma::CacheSpill, @"cache_spill");
+
+    WINQAssertEqual(WCDB::Pragma::CaseSensitiveLike, @"case_sensitive_like");
+
+    WINQAssertEqual(WCDB::Pragma::CellSizeCheck, @"cell_size_check");
+
+    WINQAssertEqual(WCDB::Pragma::CheckpointFullfsync, @"checkpoint_fullfsync");
+
+    WINQAssertEqual(WCDB::Pragma::CollationList, @"collation_list");
+
+    WINQAssertEqual(WCDB::Pragma::CompileOptions, @"compile_options");
+
+    WINQAssertEqual(WCDB::Pragma::DataVersion, @"data_version");
+
+    WINQAssertEqual(WCDB::Pragma::DatabaseList, @"database_list");
+
+    WINQAssertEqual(WCDB::Pragma::DeferForeignKeys, @"defer_foreign_keys");
+
+    WINQAssertEqual(WCDB::Pragma::Encoding, @"encoding");
+
+    WINQAssertEqual(WCDB::Pragma::ForeignKeyCheck, @"foreign_key_check");
+
+    WINQAssertEqual(WCDB::Pragma::ForeignKeyList, @"foreign_key_list");
+
+    WINQAssertEqual(WCDB::Pragma::ForeignKeys, @"foreign_keys");
+
+    WINQAssertEqual(WCDB::Pragma::FreelistCount, @"freelist_count");
+
+    WINQAssertEqual(WCDB::Pragma::Fullfsync, @"fullfsync");
+
+    WINQAssertEqual(WCDB::Pragma::FunctionList, @"function_list");
+
+    WINQAssertEqual(WCDB::Pragma::IgnoreCheckConstraints, @"ignore_check_constraints");
+
+    WINQAssertEqual(WCDB::Pragma::IncrementalVacuum, @"incremental_vacuum");
+
+    WINQAssertEqual(WCDB::Pragma::IndexInfo, @"index_info");
+
+    WINQAssertEqual(WCDB::Pragma::IndexList, @"index_list");
+
+    WINQAssertEqual(WCDB::Pragma::IndexXinfo, @"index_xinfo");
+
+    WINQAssertEqual(WCDB::Pragma::IntegrityCheck, @"integrity_check");
+
+    WINQAssertEqual(WCDB::Pragma::JournalMode, @"journal_mode");
+
+    WINQAssertEqual(WCDB::Pragma::JournalSizeLimit, @"journal_size_limit");
+
+    WINQAssertEqual(WCDB::Pragma::LegacyFileFormat, @"legacy_file_format");
+
+    WINQAssertEqual(WCDB::Pragma::LockingMode, @"locking_mode");
+
+    WINQAssertEqual(WCDB::Pragma::MaxPageCount, @"max_page_count");
+
+    WINQAssertEqual(WCDB::Pragma::MmapSize, @"mmap_size");
+
+    WINQAssertEqual(WCDB::Pragma::ModuleList, @"module_list");
+
+    WINQAssertEqual(WCDB::Pragma::Optimize, @"optimize");
+
+    WINQAssertEqual(WCDB::Pragma::PageCount, @"page_count");
+
+    WINQAssertEqual(WCDB::Pragma::PageSize, @"page_size");
+
+    WINQAssertEqual(WCDB::Pragma::ParserTrace, @"parser_trace");
+
+    WINQAssertEqual(WCDB::Pragma::PragmaList, @"pragma_list");
+
+    WINQAssertEqual(WCDB::Pragma::QueryOnly, @"query_only");
+
+    WINQAssertEqual(WCDB::Pragma::QuickCheck, @"quick_check");
+
+    WINQAssertEqual(WCDB::Pragma::ReadUncommitted, @"read_uncommitted");
+
+    WINQAssertEqual(WCDB::Pragma::RecursiveTriggers, @"recursive_triggers");
+
+    WINQAssertEqual(WCDB::Pragma::ReverseUnorderedSelects, @"reverse_unordered_selects");
+
+    WINQAssertEqual(WCDB::Pragma::SchemaVersion, @"schema_version");
+
+    WINQAssertEqual(WCDB::Pragma::SecureDelete, @"secure_delete");
+
+    WINQAssertEqual(WCDB::Pragma::ShrinkMemory, @"shrink_memory");
+
+    WINQAssertEqual(WCDB::Pragma::SoftHeapLimit, @"soft_heap_limit");
+
+    WINQAssertEqual(WCDB::Pragma::Stats, @"stats");
+
+    WINQAssertEqual(WCDB::Pragma::Synchronous, @"synchronous");
+
+    WINQAssertEqual(WCDB::Pragma::TableInfo, @"table_info");
+
+    WINQAssertEqual(WCDB::Pragma::TempStore, @"temp_store");
+
+    WINQAssertEqual(WCDB::Pragma::Threads, @"threads");
+
+    WINQAssertEqual(WCDB::Pragma::UserVersion, @"user_version");
+
+    WINQAssertEqual(WCDB::Pragma::VdbeAddoptrace, @"vdbe_addoptrace");
+
+    WINQAssertEqual(WCDB::Pragma::VdbeDebug, @"vdbe_debug");
+
+    WINQAssertEqual(WCDB::Pragma::VdbeListing, @"vdbe_listing");
+
+    WINQAssertEqual(WCDB::Pragma::VdbeTrace, @"vdbe_trace");
+
+    WINQAssertEqual(WCDB::Pragma::WalAutocheckpoint, @"wal_autocheckpoint");
+
+    WINQAssertEqual(WCDB::Pragma::WalCheckpoint, @"wal_checkpoint");
+
+    WINQAssertEqual(WCDB::Pragma::WritableSchema, @"writable_schema");
+}
+
+- (void)testPragmaSQLCipher
+{
+    WINQAssertEqual(WCDB::Pragma::Key, @"key");
+
+    WINQAssertEqual(WCDB::Pragma::Cipher, @"cipher");
+
+    WINQAssertEqual(WCDB::Pragma::KdfIter, @"kdf_iter");
+
+    WINQAssertEqual(WCDB::Pragma::CipherDefaultKdfIter, @"cipher_default_kdf_iter");
+
+    WINQAssertEqual(WCDB::Pragma::CipherPageSize, @"cipher_page_size");
+
+    WINQAssertEqual(WCDB::Pragma::CipherDefaultPageSize, @"cipher_default_page_size");
+
+    WINQAssertEqual(WCDB::Pragma::CipherUseHmac, @"cipher_use_hmac");
+
+    WINQAssertEqual(WCDB::Pragma::CipherDefaultUseHmac, @"cipher_default_use_hmac");
+
+    WINQAssertEqual(WCDB::Pragma::Rekey, @"rekey");
+
+    WINQAssertEqual(WCDB::Pragma::CipherMigrate, @"cipher_migrate");
+
+    WINQAssertEqual(WCDB::Pragma::CipherProfile, @"cipher_profile");
+
+    WINQAssertEqual(WCDB::Pragma::CipherAddRandom, @"cipher_add_random");
+
+    WINQAssertEqual(WCDB::Pragma::CipherProvider, @"cipher_provider");
+
+    WINQAssertEqual(WCDB::Pragma::CipherProviderVersion, @"cipher_provider_version");
+
+    WINQAssertEqual(WCDB::Pragma::CipherVersion, @"cipher_version");
 }
 
 @end
