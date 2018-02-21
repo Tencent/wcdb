bed1918089f1dd1334b81c41aa28b496d9b868e6
sanhuazhang <sanhuazhang@tencent.com>
2018-02-21 13:46:35 +0800

    Tests for StatementRollback

--- objc/testcase/test/winq/WTCStatementRollbackTests.mm
+++ objc/testcase/test/winq/WTCStatementRollbackTests.mm
@@ -18,10 +18,9 @@
  * limitations under the License.
  */
 
-#import "WTCAssert.h"
-#import <WINQ/abstract.h>
+#import "WTCWINQTestCase.h"
 
-@interface WTCStatementRollbackTests : XCTestCase
+@interface WTCStatementRollbackTests : WTCWINQTestCase
 
 @end
 
@@ -29,6 +28,13 @@
 
 - (void)testStatementRollback
 {
+    WINQAssertEqual(WCDB::StatementRollback()
+                        .rollbackTo(self.class.savepointName),
+                    @"ROLLBACK TO testSavepoint");
+
+    WINQAssertEqual(WCDB::StatementRollback()
+                        .rollback(),
+                    @"ROLLBACK");
 }
 
 @end
