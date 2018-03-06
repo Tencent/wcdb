ea75ffd574ce37a3ecd6df90862115783f9b644a
sanhuazhang <sanhuazhang@tencent.com>
2018-03-06 19:09:10 +0800

    Revert mis commit

--- tools/codeformatting/code-formatting.sh
+++ tools/codeformatting/code-formatting.sh
@@ -7,11 +7,11 @@ swift=$root/swift
 xctemplate=$root/tools/templates/xctemplates
 
 find $objc \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.mm" -or -name "*.m" \) | xargs clang-format -i
-#find $repair \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.c" \) | xargs clang-format -i
-#find $swift \( -name "*.h" -or -name "*.c" \) | xargs clang-format -i
-#if which swiftlint >/dev/null; then
-#    swiftlint autocorrect --path $swift
-#    swiftlint autocorrect --path $xctemplate
-#else
-#    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-#fi
+find $repair \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.c" \) | xargs clang-format -i
+find $swift \( -name "*.h" -or -name "*.c" \) | xargs clang-format -i
+if which swiftlint >/dev/null; then
+    swiftlint autocorrect --path $swift
+    swiftlint autocorrect --path $xctemplate
+else
+    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+fi
