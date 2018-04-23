730baa5f5f9944273c30f807a76314156607ec76
sanhuazhang <sanhuazhang@tencent.com>
2018-04-23 15:01:34 +0800

    Remove wrong visibility

--- objc/source/abstract/WINQ/ColumnType.hpp
+++ objc/source/abstract/WINQ/ColumnType.hpp
@@ -28,8 +28,6 @@
 #include <type_traits>
 #include <vector>
 
-#pragma GCC visibility push(hidden)
-
 namespace WCDB {
 
 typedef Lang::ColumnType ColumnType;
@@ -308,7 +306,5 @@ public:
 };
 
 } //namespace WCDB
-
-#pragma GCC visibility pop
 
 #endif /* ColumnType_hpp */
