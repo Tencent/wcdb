20b1ef78058a28e9fd02ed5f2a9dd04a01c56214
sanhuazhang <sanhuazhang@tencent.com>
2018-05-19 17:37:51 +0800

    Update formating tool

--- tools/code_formatting/code_formatting_objc.sh
+++ tools/code_formatting/code_formatting_objc.sh
@@ -3,6 +3,8 @@
 root=`pwd`/../../
 objc=$root/objc
 repair=$root/repair
+common=$root/common
 
 find $objc \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.mm" -or -name "*.m" \) | xargs clang-format -i
 find $repair \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.c" \) | xargs clang-format -i
+find $common \( -name "*.cpp" -or -name "*.hpp" -or -name "*.h" -or -name "*.c" \) | xargs clang-format -i
