4ae50d9996972697ee832e694937c4683ff36d27
sanhuazhang <sanhuazhang@tencent.com>
2018-06-07 20:50:41 +0800

    Remove log

--- objc/testcase/test/database/DatabaseTests+File.mm
+++ objc/testcase/test/database/DatabaseTests+File.mm
@@ -55,8 +55,6 @@
     //Then
     XCTAssertTrue(([_database.paths isEqualToObjects:expectedPaths
                                       withComparator:^BOOL(NSString *lhs, NSString *rhs) {
-                                        NSLog(@"lhs: %@", lhs);
-                                        NSLog(@"rhs: %@", rhs);
                                         return [lhs isEqualToString:rhs];
                                       }]));
 }
