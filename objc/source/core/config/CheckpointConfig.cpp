5fa641e6fb4ba608287f889a46e67efdf7a5b0a8
sanhuazhang <sanhuazhang@tencent.com>
2018-06-23 17:14:26 +0800

    Refactor

--- objc/source/core/config/CheckpointConfig.cpp
+++ objc/source/core/config/CheckpointConfig.cpp
@@ -78,10 +78,9 @@ void CheckpointConfig::loop()
 
 void CheckpointConfig::onTimed(const std::string &path, const int &frames) const
 {
-    //TODO
-    static std::atomic<bool> s_exit(false);
-    atexit([]() { s_exit.store(true); });
-    if (s_exit.load()) {
+    static std::atomic<bool> *s_exit = new std::atomic<bool>(false);
+    atexit([]() { s_exit->store(true); });
+    if (s_exit->load()) {
         return;
     }
 
