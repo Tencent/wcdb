785d929552d2a50f7531300e2a15b537fbde0535
sanhuazhang <sanhua.zh@foxmail.com>
2018-07-25 19:57:44 +0800

    Unpassed migration benchmark

--- objc/testcase/benchmark/MigrationBenchmark.mm
+++ objc/testcase/benchmark/MigrationBenchmark.mm
@@ -20,7 +20,6 @@
 
 #import "BenchmarkCommon.h"
 
-//TODO
 @interface MigrationBenchmark : Benchmark
 
 @end
@@ -28,16 +27,120 @@
 //Note that nothing is different between a normal database and a migration database after migrated, tests here are not for the best performance but for a tolerable performance.
 @implementation MigrationBenchmark
 
+- (void)setUpDatabase
+{
+    NSString *tableName = [self getTableNameWithIndex:0];
+    NSString *migratedTableName = [tableName stringByAppendingString:@"_migrated"];
+    WCTMigrationInfo *info = [[WCTMigrationInfo alloc] initWithTargetTable:migratedTableName fromSourceTable:tableName];
+    self.database = [[WCTMigrationDatabase alloc] initWithPath:self.recommendedPath andInfo:info];
+}
+
 - (void)test_read_within_migration
 {
+    NSString *tableName = [self getTableNameWithIndex:0];
+    __block NSArray<BenchmarkObject *> *results = nil;
+
+    [self
+    measure:^{
+        results = [self.database getObjectsOfClass:BenchmarkObject.class fromTable:tableName];
+    }
+    setUp:^{
+        [self setUpDatabase];
+        [self setUpWithPreCreateTable:1];
+        [self setUpWithPreCreateObject:self.config.readCount];
+        [self setUpWithPreInsertObjects:self.objects intoTable:tableName];
+
+        // migrate half the number of objects
+        WCTMigrationDatabase *migrationDatabase = (WCTMigrationDatabase *) self.database;
+        migrationDatabase.migrateRowPerStep = self.config.readCount / 2;
+        BOOL done;
+        XCTAssertTrue([migrationDatabase stepMigration:done]);
+        XCTAssertFalse(done);
+
+        [self tearDownDatabaseCache];
+        [self setUpDatabaseCache];
+    }
+    tearDown:^{
+        results = nil;
+        [self tearDownDatabase];
+    }
+    checkCorrectness:^{
+        XCTAssertEqual(results.count, self.config.readCount);
+        XCTAssertTrue([results isEqualToBenchmarkObjects:self.objects]);
+    }];
 }
 
 - (void)test_write_within_migration
 {
+    NSString *tableName = [self getTableNameWithIndex:0];
+    __block WCTDatabase *database = nil;
+    __block NSArray<BenchmarkObject *> *objects = nil;
+
+    [self
+    measure:^{
+        for (BenchmarkObject *object in objects) {
+            [database insertObject:object intoTable:tableName];
+        }
+    }
+    setUp:^{
+        [self setUpDatabase];
+        [self setUpWithPreCreateTable:1];
+        [self setUpWithPreCreateObject:self.config.writeCount];
+
+        // migrate half the number of objects
+        WCTMigrationDatabase *migrationDatabase = (WCTMigrationDatabase *) self.database;
+        migrationDatabase.migrateRowPerStep = self.config.writeCount / 2;
+        BOOL done;
+        XCTAssertTrue([migrationDatabase stepMigration:done]);
+        XCTAssertFalse(done);
+
+        [self tearDownDatabaseCache];
+        [self setUpDatabaseCache];
+        database = self.database;
+        objects = self.objects;
+    }
+    tearDown:^{
+        database = nil;
+        objects = nil;
+        [self tearDownDatabase];
+    }
+    checkCorrectness:^{
+        NSArray<BenchmarkObject *> *objects = [self.database getObjectsOfClass:BenchmarkObject.class fromTable:tableName orderBy:BenchmarkObject.key.asOrder(WCTOrderedAscending)];
+        XCTAssertTrue([objects isEqualToBenchmarkObjects:self.objects]);
+        XCTAssertEqual(objects.count, self.config.writeCount);
+    }];
 }
 
 - (void)test_batch_write_within_migration
 {
+    NSString *tableName = [self getTableNameWithIndex:0];
+    [self
+    measure:^{
+        [self.database insertObjects:self.objects intoTable:tableName];
+    }
+    setUp:^{
+        [self setUpDatabase];
+        [self setUpWithPreCreateTable:1];
+        [self setUpWithPreCreateObject:self.config.batchWriteCount];
+
+        // migrate half the number of objects
+        WCTMigrationDatabase *migrationDatabase = (WCTMigrationDatabase *) self.database;
+        migrationDatabase.migrateRowPerStep = self.config.batchWriteCount / 2;
+        BOOL done;
+        XCTAssertTrue([migrationDatabase stepMigration:done]);
+        XCTAssertFalse(done);
+
+        [self tearDownDatabaseCache];
+        [self setUpDatabaseCache];
+    }
+    tearDown:^{
+        [self tearDownDatabase];
+    }
+    checkCorrectness:^{
+        NSArray<BenchmarkObject *> *objects = [self.database getObjectsOfClass:BenchmarkObject.class fromTable:tableName orderBy:BenchmarkObject.key.asOrder(WCTOrderedAscending)];
+        XCTAssertTrue([objects isEqualToBenchmarkObjects:self.objects]);
+        XCTAssertEqual(objects.count, self.config.batchWriteCount);
+    }];
 }
 
 @end
