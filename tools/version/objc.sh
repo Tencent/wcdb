7cf9a3b378e5af2c0b22d3f6ad9945a048b296bd
sanhuazhang <sanhua.zh@foxmail.com>
2018-09-30 16:34:06 +0800

    New version script

--- tools/version/objc.sh
+++ tools/version/objc.sh
@@ -1,40 +1,16 @@
 #!/bin/bash
 
 root=`git rev-parse --show-toplevel`
+version=`defaults read ${root}/objc/support/Info.plist CFBundleShortVersionString`
+commitHash=`git rev-parse HEAD`
 buildTimestamp=`date +%s`
-buildDate=`date -r ${buildTimestamp} +"%Y-%m-%d %H:%M:%S UTC%z"`
-commitID=`git rev-parse HEAD`
-version=`defaults read /Users/sanhuazhang/Developer/Github/wcdb/objc/Info.plist CFBundleShortVersionString`
+buildTime=`date -r ${buildTimestamp} +"%Y-%m-%d %H:%M:%S UTC%z"`
 
-echo """/*
- * Tencent is pleased to support the open source community by making
- * WCDB available.
- *
- * Copyright (C) 2017 THL A29 Limited, a Tencent company.
- * All rights reserved.
- *
- * Licensed under the BSD 3-Clause License (the "License"); you may not use
- * this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- *       https://opensource.org/licenses/BSD-3-Clause
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+source=${root}/tools/version/Version.h
+target=${root}/common/Version.h
 
-#ifndef Version_h
-#define Version_h
-
-/* DO NOT CHANGE IT MANUALLY */
-
-#define WCDB_VERSION \"${version}\"
-#define WCDB_COMMIT_HASH \"${commitID}\"
-#define WCDB_BUILD_TIMESTAMP ${buildTimestamp}
-#define WCDB_BUILD_TIME \"${buildDate}\"
-
-#endif /* Version_h */
-""" > ${root}/common/Version.h
+sed -e "s/WCDB_VERSION_PLACEHOLDER/${version}/g" \
+    -e "s/WCDB_COMMIT_HASH_PLACEHOLDER/${commitHash}/g" \
+    -e "s/WCDB_BUILD_TIMESTAMP_PLACEHOLDER/${buildTimestamp}/g" \
+    -e "s/WCDB_BUILD_TIME_PLACEHOLDER/${buildTime}/g" \
+    ${source} > ${target}
