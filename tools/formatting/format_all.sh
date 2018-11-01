c8eb882e0d8c13d44d7ea4a81fd0df17ed5655b7
sanhuazhang <sanhua.zh@foxmail.com>
2018-11-01 18:24:42 +0800

    Git hooks for checking license

--- tools/formatting/format_all.sh
+++ tools/formatting/format_all.sh
@@ -3,15 +3,19 @@
 root=`git rev-parse --show-toplevel`
 
 paths=(common repair objc swift tools/templates tools/version)
-formatter=$root/tools/formatting/format.sh
+codeFormatter=$root/tools/formatting/format_code.sh
 fileHeaderFormatter=$root/tools/formatting/format_file_header.sh
+licenseChecker=$root/tools/formatting/check_license.sh
 
 for path in ${paths[@]}; do
     fullpath=$root/$path
     for file in `find $fullpath \( -name "*.h" -or -name "*.hpp" -or -name "*.c" -or -name "*.cpp" -or -name "*.m" -or -name "*.mm" -or -name "*.swift" \)`; do
-        if ! (sh $fileHeaderFormatter --noadd $file); then
+        if ! (sh $codeFormatter --noadd $file); then
             exit 1
         fi
         fileHeaderFormatter $file
+        if ! (sh $licenseChecker $file); then
+            exit 1
+        fi
     done
 done
