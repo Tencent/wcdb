28eba334bc90538c7369eebdaadee1af77ed5a8a
sanhuazhang <sanhua.zh@foxmail.com>
2019-01-24 18:01:12 +0800

    Add license

--- objc/testcase/winq/WINQAssertion.mm
+++ objc/testcase/winq/WINQAssertion.mm
@@ -1,10 +1,22 @@
-//
-//  WINQAssertion.m
-//  WINQ
-//
-//  Created by sanhuazhang(张三华) on 2019/1/7.
-//  Copyright © 2019 sanhuazhang. All rights reserved.
-//
+/*
+ * Tencent is pleased to support the open source community by making
+ * WCDB available.
+ *
+ * Copyright (C) 2017 THL A29 Limited, a Tencent company.
+ * All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *       https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "WINQAssertion.h"
 
