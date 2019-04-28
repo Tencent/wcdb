feeb13f72454b91a573c62276477ac9ea26d0408
sanhuazhang <sanhuazhang@tencent.com>
2019-04-28 10:44:30 +0800

    更新脚本为 merge

--- WeChat/pull_from_github.sh
+++ WeChat/pull_from_github.sh
@@ -5,8 +5,7 @@ branch="WeChat/iOS"
 githubBranch="pre_v1.1.0"
 git checkout ${branch}
 git pull --rebase github ${githubBranch}:${githubBranch}
-git rebase ${githubBranch}
-git rebase origin/${branch}
+git merge ${githubBranch}
 
 sh ${root}/tools/version/bump.sh
 git add ${root}/objc/source/utility/Version.h
