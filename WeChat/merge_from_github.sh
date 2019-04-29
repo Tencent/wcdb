c8a486bbd4eb0e138e07f1de0e9d572dea64d3ee
sanhuazhang <sanhuazhang@tencent.com>
2019-04-29 16:37:23 +0800

    更新脚本

--- WeChat/merge_from_github.sh
+++ WeChat/merge_from_github.sh
@@ -4,8 +4,11 @@ root=`git rev-parse --show-toplevel`
 branch="WeChat/iOS"
 githubBranch="pre_v1.1.0"
 git checkout ${githubBranch}
+git submodule update sqlcipher
 git pull --rebase github ${githubBranch}:${githubBranch}
+git submodule update sqlcipher
 git checkout ${branch}
+git submodule update sqlcipher
 git merge ${githubBranch}
 
 sh ${root}/tools/version/bump.sh
