989a985fb13d15ae8e11efd44da04ace7b225264
sanhuazhang <sanhuazhang@tencent.com>
2019-05-15 16:04:07 +0800

    更新脚本，不检查 clang version

--- WeChat/publish_to_conan.sh
+++ WeChat/publish_to_conan.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 if brew ls conan > /dev/null; then
     echo "conan installed"
 else
@@ -6,44 +7,57 @@ else
     brew install conan
 fi
 
+echo "Usage: publish_conan.sh -r [remote conan,default conan-mmclient] -p platform[ios/android] -v version[1.0.0] -d \"diretory to resources\" -n \"project name\""
+
 dst=''
 name=''
 plat=''
 version=''
+remote='conan-mmclient'
 force=false
 publish_ios() {
     echo "conan ios, version: $version"
-    conan user -p releaser@123 -r conan-mmclient releaser
-    conan remote add conan-mmclient http://conan.weixin.oa.com/artifactory/api/conan/conan-mmclient
+    conan remote add ${remote} http://conan.weixin.oa.com/artifactory/api/conan/${remote}
+    conan user -p mars@123 -r ${remote} mars
     cd ${dst}
     conan new ${name}-ios/${version} --bare
+    cat conanfile.py| sed 's/\"compiler\", //g' > ./conanfile2.py
+    mv -f conanfile2.py conanfile.py
     conan export-pkg . ${name}-ios/${version}@conan/stable --force
     if [[ $force == true ]]
     then
         echo "force upload"
-        conan upload ${name}-ios/${version}@conan/stable -r conan-mmclient --all --force
+        conan upload ${name}-ios/${version}@conan/stable -r ${remote} --all --force
     else
-        conan upload ${name}-ios/${version}@conan/stable -r conan-mmclient --all --no-overwrite
+        conan upload ${name}-ios/${version}@conan/stable -r ${remote} --all --no-overwrite
+    fi
+    if [[ $? -ne 0 ]]
+    then
+	    exit 1
     fi
 }
 
 publish_android() {
     echo "conan android, version: $version"
-    conan user -p releaser@123 -r conan-mmclient releaser
-    conan remote add conan-mmclient http://conan.weixin.oa.com/artifactory/api/conan/conan-mmclient
+    conan remote add ${remote} http://conan.weixin.oa.com/artifactory/api/conan/${remote}
+    conan user -p mars@123 -r ${remote} mars
     cd ${dst}
     conan new ${name}-android/${version} --bare
     conan export-pkg . ${name}-android/${version}@conan/stable --force
     if [[ $force == true ]]
     then
         echo "force upload"
-        conan upload ${name}-android/${version}@conan/stable -r conan-mmclient --all --force
+        conan upload ${name}-android/${version}@conan/stable -r ${remote} --all --force
     else
-        conan upload ${name}-android/${version}@conan/stable -r conan-mmclient --all --no-overwrite
+        conan upload ${name}-android/${version}@conan/stable -r ${remote} --all --no-overwrite
+    fi
+    if [[ $? -ne 0 ]]
+    then
+	    exit 1
     fi
 }
 
-while getopts "p:v:d:n:f" optname
+while getopts "r:p:v:d:n:f" optname
 do
     case "$optname" in
         p)
@@ -61,6 +75,9 @@ do
         n)
             name=$OPTARG
             ;;
+        r)
+            remote=$OPTARG
+            ;;
         ?)
             echo "argument error"
             exit 1
@@ -68,10 +85,10 @@ do
     esac
 done
 echo "platform: $plat, version:$version, force:$force, dst:$dst, name:$name"
-if [ $plat == "ios" ]
+if [[ $plat == "ios" ]]
 then
     publish_ios
-elif [ $plat == "android" ]
+elif [[ $plat == "android" ]]
 then
     publish_android
 fi
