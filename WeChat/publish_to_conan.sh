a083ad6d0c55fb7d8f678d456236c6aa1469febe
qiuwenchen <qiuwenchen@tencent.com>
2022-01-07 18:51:14 +0800

    conan update

--- WeChat/publish_to_conan.sh
+++ WeChat/publish_to_conan.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-
-if brew ls conan > /dev/null; then
-    echo "conan installed"
-else
-    echo "conan not installed"
-    brew install conan
-fi
-
-echo "Usage: publish_conan.sh -r [remote conan,default tencent-conan] -p platform[ios/android] -v version[1.0.0] -d \"diretory to resources\" -n \"project name\""
+echo "Usage: publish_conan.sh -r [remote conan,default tencent-conan] -p platform[ios/android/applesilicon/mac] -v version[1.0.0] -d \"diretory to resources\" -n \"project name\""
 
 dst=''
 name=''
@@ -17,43 +9,115 @@ remote='tencent-conan'
 force=false
 publish_ios() {
     echo "conan ios, version: $version"
-    conan remote add ${remote} http://conan.weixin.oa.com/artifactory/api/conan/${remote}
+    conan remote add ${remote} http://mirrors.tencent.com/repository/conan/conan-local
     conan user -p b0e97e4237ca11ec9d426c92bf47000d -r ${remote} g_wxg_upload
     cd ${dst}
     conan new ${name}-ios/${version} --bare
-    cat conanfile.py| sed 's/\"compiler\", //g' > ./conanfile2.py
+    # cat conanfile.py| sed 's/\"compiler\", //g' > ./conanfile2.py
+    cat conanfile.py | sed -e '/settings/d' > ./conanfile2.py
     mv -f conanfile2.py conanfile.py
     conan export-pkg . ${name}-ios/${version}@conan/stable --force
-    if [[ $force == true ]]
+    if [ $force = true ]
     then
         echo "force upload"
         conan upload ${name}-ios/${version}@conan/stable -r ${remote} --all --force
     else
         conan upload ${name}-ios/${version}@conan/stable -r ${remote} --all --no-overwrite
     fi
-    if [[ $? -ne 0 ]]
+    if [ $? -ne 0 ]
     then
-	    exit 1
+        exit 1
+    fi
+}
+
+publish_watch() {
+    echo "conan watch, version: $version"
+    conan remote add ${remote} http://mirrors.tencent.com/repository/conan/conan-local
+    conan user -p b0e97e4237ca11ec9d426c92bf47000d -r ${remote} g_wxg_upload
+    cd ${dst}
+    conan new ${name}-watch/${version} --bare
+    # cat conanfile.py| sed 's/\"compiler\", //g' > ./conanfile2.py
+    cat conanfile.py | sed -e '/settings/d' > ./conanfile2.py
+    mv -f conanfile2.py conanfile.py
+    conan export-pkg . ${name}-watch/${version}@conan/stable --force
+    if [ $force = true ]
+    then
+        echo "force upload"
+        conan upload ${name}-watch/${version}@conan/stable -r ${remote} --all --force
+    else
+        conan upload ${name}-watch/${version}@conan/stable -r ${remote} --all --no-overwrite
+    fi
+    if [ $? -ne 0 ]
+    then
+        exit 1
     fi
 }
 
 publish_android() {
     echo "conan android, version: $version"
-    conan remote add ${remote} http://conan.weixin.oa.com/artifactory/api/conan/${remote}
+    conan remote add ${remote} http://mirrors.tencent.com/repository/conan/conan-local
     conan user -p b0e97e4237ca11ec9d426c92bf47000d -r ${remote} g_wxg_upload
     cd ${dst}
     conan new ${name}-android/${version} --bare
+    cat conanfile.py | sed -e '/settings/d' > ./conanfile2.py
+    mv -f conanfile2.py conanfile.py
     conan export-pkg . ${name}-android/${version}@conan/stable --force
-    if [[ $force == true ]]
+    if [ $force = true ]
     then
         echo "force upload"
         conan upload ${name}-android/${version}@conan/stable -r ${remote} --all --force
     else
         conan upload ${name}-android/${version}@conan/stable -r ${remote} --all --no-overwrite
     fi
-    if [[ $? -ne 0 ]]
+    if [ $? -ne 0 ]
     then
-	    exit 1
+        exit 1
+    fi
+}
+
+publish_applesilicon() {
+    echo "conan applesilicon, version: $version"
+    conan remote add ${remote} http://mirrors.tencent.com/repository/conan/conan-local
+    conan user -p b0e97e4237ca11ec9d426c92bf47000d -r ${remote} g_wxg_upload
+    cd ${dst}
+    conan new ${name}-applesilicon/${version} --bare
+    sed -i "" 's/self.copy("\*")/self.copy("\*", symlinks=True)/g' conanfile.py 
+    cat conanfile.py | sed -e '/settings/d' > ./conanfile2.py
+    mv -f conanfile2.py conanfile.py
+    conan export-pkg . ${name}-applesilicon/${version}@conan/stable --force
+    if [ $force = true ]
+    then
+        echo "force upload"
+        conan upload ${name}-applesilicon/${version}@conan/stable -r ${remote} --all --force
+    else
+        conan upload ${name}-applesilicon/${version}@conan/stable -r ${remote} --all --no-overwrite
+    fi
+    if [ $? -ne 0 ]
+    then
+        exit 1
+    fi
+}
+
+publish_mac() {
+    echo "conan mac, version: $version"
+    conan remote add ${remote} http://mirrors.tencent.com/repository/conan/conan-local
+    conan user -p b0e97e4237ca11ec9d426c92bf47000d -r ${remote} g_wxg_upload
+    cd ${dst} || exit 1
+    conan new ${name}-mac/${version} --bare
+    sed -i "" 's/self.copy("\*")/self.copy("\*", symlinks=True)/g' conanfile.py 
+    cat conanfile.py | sed -e '/settings/d' > ./conanfile2.py
+    mv -f conanfile2.py conanfile.py
+    conan export-pkg . ${name}-mac/${version}@conan/stable --force
+    if [ $force = true ]
+    then
+        echo "force upload"
+        conan upload ${name}-mac/${version}@conan/stable -r ${remote} --all --force
+    else
+        conan upload ${name}-mac/${version}@conan/stable -r ${remote} --all --no-overwrite
+    fi
+    if [ $? -ne 0 ]
+    then
+        exit 1
     fi
 }
 
@@ -85,12 +149,21 @@ do
     esac
 done
 echo "platform: $plat, version:$version, force:$force, dst:$dst, name:$name"
-if [[ $plat == "ios" ]]
+if [ $plat = "ios" ]
 then
     publish_ios
-elif [[ $plat == "android" ]]
+elif [ $plat = "watch" ]
+then
+    publish_watch
+elif [ $plat = "android" ]
 then
     publish_android
+elif [ $plat = "applesilicon" ]
+then
+    publish_applesilicon
+elif [ $plat = "mac" ]
+then
+    publish_mac
 fi
 
 exit 0
