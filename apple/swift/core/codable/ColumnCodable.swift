da40b6d5aad78f901923bbd928d769e71912b81e
qiuwenchen <qiuwenchen@tencent.com>
2022-09-29 17:45:12 +0800

    Explicit state conformance of base protocol in conditional conformances

--- apple/swift/core/codable/ColumnCodable.swift
+++ apple/swift/core/codable/ColumnCodable.swift
@@ -38,4 +38,4 @@ public protocol ColumnDecodable: Decodable, ColumnCodableBase {
     init?(with value: Value)
 }
 
-public typealias ColumnCodable = ColumnCodableBase & ColumnEncodable & ColumnDecodable
+public typealias ColumnCodable = ColumnEncodable & ColumnDecodable
