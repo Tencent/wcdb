ed4146388b0c028d2930975d6dcbe3acd7aae9e4
cqwicqw <qiuwenchen@tencent.com>
2023-01-18 16:35:10 +0800

    Remove cache of SQL when modify statement through iteration

--- apple/common/core/sqlite/HandleStatement.cpp
+++ apple/common/core/sqlite/HandleStatement.cpp
@@ -216,7 +216,7 @@ bool HandleStatement::tryExtractColumnInfo(const Statement &statement,
     bool findTable = !tableSpecified;
     Statement copyStatement = statement;
     bool invalidStatement = false;
-    
+
     copyStatement.syntax();
     copyStatement.iterate([&](Syntax::Identifier &identifier, bool &stop) {
         StringView curTableName;
