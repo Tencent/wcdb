29649a39e955833f9de3145c1728e7f39f335340
qiuwenchen <qiuwenchen@tencent.com>
2023-03-23 10:03:13 +0800

    Code format

--- src/bridge/winqbridge/identifier/LiteralValueBridge.cpp
+++ src/bridge/winqbridge/identifier/LiteralValueBridge.cpp
@@ -29,12 +29,14 @@
 
 CPPLiteralValue WCDBLiteralValueCreateWithInt64(long long value)
 {
-    return WCDBCreateCPPBridgedObject(CPPLiteralValue, new WCDB::LiteralValue((int64_t)value));
+    return WCDBCreateCPPBridgedObject(CPPLiteralValue,
+                                      new WCDB::LiteralValue((int64_t) value));
 }
 
 CPPLiteralValue WCDBLiteralValueCreateWithUInt64(unsigned long long value)
 {
-    return WCDBCreateCPPBridgedObject(CPPLiteralValue, new WCDB::LiteralValue((uint64_t)value));
+    return WCDBCreateCPPBridgedObject(CPPLiteralValue,
+                                      new WCDB::LiteralValue((uint64_t) value));
 }
 
 CPPLiteralValue WCDBLiteralValueCreateWithDouble(double value)
