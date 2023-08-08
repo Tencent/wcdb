5764392a92adc0a25bf93d86f4486c633677389d
johnwhe <johnwhe@tencent.com>
2023-08-08 14:55:53 +0800

    Update gradle plugin

--- src/java/gradle-plugin/src/main/groovy/com/tencent/wcdb/gradle/WCDBPlugin.groovy
+++ src/java/gradle-plugin/src/main/groovy/com/tencent/wcdb/gradle/WCDBPlugin.groovy
@@ -21,6 +21,27 @@ class WCDBPlugin implements Plugin<Project> {
         project.task("generateWcdbOrm", type: JavaCompile) {
             source = sourceSets.wcdb.allJava
             classpath = project.buildscript.configurations.classpath
+            options.annotationProcessorPath = project.buildscript.configurations.classpath
+            options.compilerArgs.addAll(["-proc:only", "-implicit:none"])
+            destinationDir = new File(project.buildDir, "generated/source/wcdb")
+        }
+
+        def android = project.extensions.android
+
+        android.sourceSets {
+            main {
+                java { srcDir 'src/wcdb' }
+            }
+        }
+
+        if (project.plugins.hasPlugin(AppPlugin)) {
+            android.applicationVariants.all { variant ->
+                variant.registerJavaGeneratingTask(generateWcdbOrm, new File(project.buildDir, "generated/source/wcdb"))
+            }
+        } else {
+            android.libraryVariants.all { variant ->
+                variant.registerJavaGeneratingTask(generateWcdbOrm, new File(project.buildDir, "generated/source/wcdb"))
+            }
         }
     }
 }
