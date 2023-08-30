7cc1af9e8a8e18961f0bc9a65bde70dc2ed4c455
johnwhe <johnwhe@tencent.com>
2023-08-30 19:36:22 +0800

    Fix version script

--- WeChat/replace_version.sh
+++ WeChat/replace_version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 basedir=$(dirname "$0")
-awk -f "$basedir/parse_version.awk" -v date="$(date +'%Y%m%d')" -v rev_short="$(git rev-parse --short=9 HEAD)" "$basedir/wconan_package.yaml" > "$basedir/../VERSION"
+pushd "$basedir"
+awk -f parse_version.awk -v date="$(date +'%Y%m%d')" -v rev_short="$(git rev-parse --short=9 HEAD)" wconan_package.yaml > ../VERSION
+popd
