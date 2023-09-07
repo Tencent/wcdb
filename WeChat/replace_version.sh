008cab0d6eb46203c0788b9e11d11a01a7171aa3
qiuwenchen <qiuwenchen@tencent.com>
2023-09-07 15:49:29 +0800

    Update commit length

--- WeChat/replace_version.sh
+++ WeChat/replace_version.sh
@@ -2,5 +2,5 @@
 
 basedir=$(dirname "$0")
 pushd "$basedir"
-awk -f parse_version.awk -v date="$(date +'%Y%m%d')" -v rev_short="$(git rev-parse --short=9 HEAD)" wconan_package.yaml > ../VERSION
+awk -f parse_version.awk -v date="$(date +'%Y%m%d')" -v rev_short="$(git rev-parse --short=8 HEAD)" wconan_package.yaml > ../VERSION
 popd
