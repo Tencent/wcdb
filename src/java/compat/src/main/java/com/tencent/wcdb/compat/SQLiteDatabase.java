0658aa02bd7d4451f8470353479fd645bce30163
qiuwenchen <qiuwenchen@tencent.com>
2024-10-14 17:23:31 +0800

    Revert "fix fatal WCDBException when error occurs and statement is not finalized"

--- src/java/compat/src/main/java/com/tencent/wcdb/compat/SQLiteDatabase.java
+++ src/java/compat/src/main/java/com/tencent/wcdb/compat/SQLiteDatabase.java
@@ -629,11 +629,10 @@ public final class SQLiteDatabase extends SQLiteClosable {
      */
     public Cursor rawQuery(String sql, Object[] selectionArgs,
                            CancellationSignal cancellationSignal) {
-        PreparedStatement stmt = null;
         try (Handle handle = mDB.getHandle(false)) {
             DatabaseUtils.bindCancellationSignal(handle, cancellationSignal);
 
-            stmt = handle.preparedWithMainStatement(sql);
+            PreparedStatement stmt = handle.preparedWithMainStatement(sql);
             if (selectionArgs != null) {
                 int nArgs = selectionArgs.length;
                 for (int i = 0; i < nArgs; ++i) {
@@ -647,13 +646,10 @@ public final class SQLiteDatabase extends SQLiteClosable {
             }
 
             List<Value[]> result = stmt.getMultiRows();
+            stmt.finalizeStatement();
             return new ValueCursor(result, columnNames);
         } catch (WCDBInterruptException e) {
             throw (OperationCanceledException) (new OperationCanceledException().initCause(e));
-        } finally {
-            if (stmt != null) {
-                stmt.finalizeStatement();
-            }
         }
     }
 
@@ -803,20 +799,16 @@ public final class SQLiteDatabase extends SQLiteClosable {
             case CONFLICT_REPLACE: insert.orReplace(); break;
         }
         insert.columns(columns).valuesWithBindParameters(bindArgs.length);
-        PreparedStatement stmt = null;
         try (Handle handle = mDB.getHandle(true)) {
-            stmt = handle.preparedWithMainStatement(insert);
+            PreparedStatement stmt = handle.preparedWithMainStatement(insert);
             for (int i = 0; i < bindArgs.length; ++i) {
                 stmt.bindValue(new Value(bindArgs[i]), i + 1);
             }
             do {
                 stmt.step();
             } while (!stmt.isDone());
+            stmt.finalizeStatement();
             return handle.getLastInsertedRowId();
-        } finally {
-            if (stmt != null) {
-                stmt.finalizeStatement();
-            }
         }
     }
 
@@ -836,9 +828,8 @@ public final class SQLiteDatabase extends SQLiteClosable {
     public int delete(String table, String whereClause, String[] whereArgs) {
         String sql = "DELETE FROM " + table +
                 (!TextUtils.isEmpty(whereClause) ? " WHERE " + whereClause : "");
-        PreparedStatement stmt = null;
         try (Handle handle = mDB.getHandle(true)) {
-            stmt = handle.preparedWithMainStatement(sql);
+            PreparedStatement stmt = handle.preparedWithMainStatement(sql);
             if (whereArgs != null) {
                 for (int i = 0; i < whereArgs.length; ++i) {
                     stmt.bindText(whereArgs[i], i + 1);
@@ -847,11 +838,8 @@ public final class SQLiteDatabase extends SQLiteClosable {
             do {
                 stmt.step();
             } while (!stmt.isDone());
+            stmt.finalizeStatement();
             return handle.getChanges();
-        } finally {
-            if (stmt != null) {
-                stmt.finalizeStatement();
-            }
         }
     }
 
@@ -914,20 +902,16 @@ public final class SQLiteDatabase extends SQLiteClosable {
             sql.append(" WHERE ");
             sql.append(whereClause);
         }
-        PreparedStatement stmt = null;
         try (Handle handle = mDB.getHandle(true)) {
-            stmt = handle.preparedWithMainStatement(sql.toString());
+            PreparedStatement stmt = handle.preparedWithMainStatement(sql.toString());
             for (i = 0; i < bindArgsSize; ++i) {
                 stmt.bindValue(new Value(bindArgs[i]), i + 1);
             }
             do {
                 stmt.step();
             } while (!stmt.isDone());
+            stmt.finalizeStatement();
             return handle.getChanges();
-        } finally {
-            if (stmt != null) {
-                stmt.finalizeStatement();
-            }
         }
     }
 
