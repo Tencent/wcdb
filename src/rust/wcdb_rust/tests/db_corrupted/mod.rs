7985e93e5bef988d451d417b60d53b8660fc74bc
qixinbing <qixinbing@rongcloud.cn>
2025-03-11 14:41:19 +0800

    test: add test case for delete db file.

--- src/rust/wcdb_rust/tests/db_corrupted/mod.rs
+++ src/rust/wcdb_rust/tests/db_corrupted/mod.rs
@@ -1,4 +1,5 @@
 pub(crate) mod corrupted_base_test_case;
+pub(crate) mod delete_db_file_test; // 删除 db 文件用例，不可恢复
 pub(crate) mod delete_wal_shm_test; // 删除 wal 和 shm 文件用例，可恢复
 pub(crate) mod modify_db_file_test; // 修改 db 文件头部用例，可恢复
 pub(crate) mod terminated_when_write_test; // 强制终止写操作，可恢复
