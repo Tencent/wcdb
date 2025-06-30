54aff192ffe79b1f3ba7ad2e4663ba11a58776ed
qixinbing <qixinbing@rongcloud.cn>
2025-06-30 18:42:43 +0800

    feat: build.rs support mac & ios.

--- src/rust/wcdb/build.rs
+++ src/rust/wcdb/build.rs
@@ -1,16 +1,12 @@
 use std::env;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Output};
 
 fn main() {
+    run_cmd("git submodule update --init openssl sqlcipher zstd");
+
     let target = env::var("TARGET").unwrap();
-
     let dst = config_cmake(&target);
-
-    Command::new("git")
-        .arg("submodule update --init openssl sqlcipher zstd")
-        .output()
-        .expect("failed to execute cmd: git submodule update --init openssl sqlcipher zstd");
 
     println!("cargo:rerun-if-changed=cpp");
     println!("cargo:rustc-link-lib=static=sqlcipher");
@@ -31,14 +27,9 @@ fn main() {
         println!("cargo:rustc-link-lib=static=WCDB");
     } else if target.contains("ohos") || target.contains("android") || target.contains("linux") {
         let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let openssl_search_path = openssl_search_path_from_target(&target).unwrap();
+        let openssl_search_path = openssl_search_path_from_target(&target)
+            .expect(&format!("wcdb not support target: {}", target));
         let openssl_path = manifest_dir.join(openssl_search_path);
-        println!(
-            "cargo:warning=WCDB MANIFEST_DIR: {}",
-            manifest_dir.display()
-        );
-        println!("cargo:warning=WCDB crypto path: {}", openssl_path.display());
-        println!("cargo:warning=WCDB crypto exist: {}", openssl_path.exists());
         println!("cargo:rustc-link-lib=stdc++");
         println!("cargo:rustc-link-search=native={}", openssl_path.display());
         println!("cargo:rustc-link-lib=static=crypto");
@@ -52,23 +43,47 @@ fn main() {
 
 fn config_cmake(target: &str) -> PathBuf {
     let mut cmake = cmake::Config::new("../cpp");
-    if !target.contains("apple") {
-        // apple 平台会直接使用 mac 系统内置的 asm/C/C++ 编译器
-        // 非 apple 平台需要指明 asm/C/C++ 编译器
-        let cc = env::var("CC").expect(&format!("{} is not set CC", &target));
-        let cxx = env::var("CXX").expect(&format!("{} is not set CXX", &target));
 
-        // 指定 asm 编译器：zstd huf_decompress_amd64.S 是汇编
-        // 指定 C 编译器：openssl 主要是 C
-        // 指定 C++ 编译器：WCDB 主要是 C++
-        cmake
-            .define("CMAKE_ASM_COMPILER", &cc)
-            .define("CMAKE_ASM_FLAGS", "-x assembler-with-cpp")
-            .define("CMAKE_C_COMPILER", &cc)
-            .define("CMAKE_CXX_COMPILER", &cxx);
+    let mut cc = String::new();
+    let mut cxx = String::new();
+    if target.contains("apple") {
+        cc = env::var("CC").unwrap_or("/usr/bin/clang".to_string());
+        cxx = env::var("CXX").unwrap_or("/usr/bin/clang++".to_string());
+    } else {
+        cc = env::var("CC").expect(&format!("wcdb: {} is not set CC", &target));
+        cxx = env::var("CXX").expect(&format!("wcdb: {} is not set CXX", &target));
     }
-    if target.contains("android") {
-        config_cmake_for_android(target, &mut cmake);
+    cmake
+        .define("CMAKE_ASM_COMPILER", &cc) // 指定 asm 编译器：zstd huf_decompress_amd64.S 是汇编
+        .define("CMAKE_ASM_FLAGS", "-x assembler-with-cpp")
+        .define("CMAKE_C_COMPILER", &cc) // 指定 C 编译器：openssl 主要是 C
+        .define("CMAKE_CXX_COMPILER", &cxx); // 指定 C++ 编译器：WCDB 主要是 C++
+
+    if target.contains("apple") {
+        let (system_name, sdk_name, arch_name) = ios_sdk_arch_from_target(target)
+            .expect(&format!("wcdb not support target: {}", target));
+
+        let output = run_cmd(&format!("xcrun --sdk {} --show-sdk-path", sdk_name));
+        let sysroot = String::from_utf8(output.stdout).unwrap().replace("\n", "");
+
+        cmake
+            .define("CMAKE_SYSTEM_NAME", system_name)
+            .define("CMAKE_OSX_SYSROOT", &sysroot)
+            .define("CMAKE_SYSROOT", &sysroot)
+            .define("CMAKE_OSX_ARCHITECTURES", arch_name);
+    } else if target.contains("android") {
+        let toolchain_file = env::var("CMAKE_TOOLCHAIN_FILE").expect(&format!(
+            "wcdb: {} is not set CMAKE_TOOLCHAIN_FILE",
+            &target
+        ));
+        cmake.define("CMAKE_TOOLCHAIN_FILE", toolchain_file);
+
+        let abi =
+            android_abi_from_target(target).expect(&format!("wcdb: {} can not find abi", &target));
+        cmake.define("ANDROID_ABI", abi);
+
+        // ✅ 设置平台版本（例如 android-21）
+        cmake.define("ANDROID_PLATFORM", "android-21");
     }
 
     let dst = cmake
@@ -82,16 +97,17 @@ fn config_cmake(target: &str) -> PathBuf {
     dst
 }
 
-fn config_cmake_for_android(target: &str, cmake: &mut cmake::Config) {
-    let toolchain_file = env::var("CMAKE_TOOLCHAIN_FILE")
-        .expect(&format!("{} is not set CMAKE_TOOLCHAIN_FILE", &target));
-    cmake.define("CMAKE_TOOLCHAIN_FILE", toolchain_file);
-
-    let abi = android_abi_from_target(target).expect(&format!("{} can not find abi", &target));
-    cmake.define("ANDROID_ABI", abi);
-
-    // ✅ 设置平台版本（例如 android-21）
-    cmake.define("ANDROID_PLATFORM", "android-21");
+fn ios_sdk_arch_from_target(target: &str) -> Option<(&'static str, &'static str, &'static str)> {
+    match target {
+        // ios
+        "aarch64-apple-ios" => Some(("iOS", "iphoneos", "arm64")),
+        "aarch64-apple-ios-sim" => Some(("iOS", "iphonesimulator", "arm64")),
+        "x86_64-apple-ios" => Some(("iOS", "iphonesimulator", "x86_64")),
+        // mac
+        "aarch64-apple-darwin" => Some(("Darwin", "macosx", "arm64")),
+        "x86_64-apple-darwin" => Some(("Darwin", "macosx", "x86_64")),
+        _ => None,
+    }
 }
 
 fn android_abi_from_target(target: &str) -> Option<&'static str> {
@@ -119,4 +135,12 @@ fn openssl_search_path_from_target(target: &str) -> Option<&'static str> {
         "x86_64-unknown-linux-gnu" => Some("../../../tools/prebuild/openssl/linux/x86_64"),
         _ => None,
     }
+}
+
+fn run_cmd(cmd: &str) -> Output {
+    Command::new("sh")
+        .arg("-c")
+        .arg(cmd)
+        .output()
+        .expect(&format!("wcdb : failed to execute command :{}", cmd))
 }
